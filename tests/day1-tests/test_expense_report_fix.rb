284d9200add4d388af0742cebb59f25b9b53679e
Yassen Damyanov <yd@itlabs.bg>
2021-11-24 11:45:02 +0200

    Refactor further Day 1 solution
     - Move scan_pairs() and scan_triads() out of Array and into
       ExspenseReportFix;
    
     - Apply 'extend self' idiom to ExspenseReportFix module;
     - Replace "clever" method invocation with an simple 'if';
     - Refactor unit tests.

--- tests/day1-tests/test_expense_report_fix.rb
+++ tests/day1-tests/test_expense_report_fix.rb
@@ -4,7 +4,7 @@ require 'test/unit'
 require_relative '../common'
 require_relative SRC_DIR + 'day1/expense_report_fix'
 
-# Need to isolate in a module to avoid constants' name collisions/side effects
+# Isolate tests in a module to avoid constants' name collisions/side effects
 module TestExpenseReportWrapper
 
   MY_DIR = File.expand_path File.dirname(__FILE__)
@@ -14,41 +14,81 @@ module TestExpenseReportWrapper
   REFERENCE_INPUT = AocUtils.file_to_array(REF_INPUT_FILE)
   PUZZLE_INPUT = AocUtils.file_to_array(PUZZLE_INPUT_FILE)
 
-  class TestExpenseFixesDay1 < Test::Unit::TestCase
+  class TestArrayExtentions < Test::Unit::TestCase
+
+    def test_scan_pairs__size_3
+      res = []
+      ExpenseReportFix.scan_pair_combinations(3) { |i, j| res << [i, j] }
+      assert_equal [[0, 1], [0, 2], [1, 2]], res
+    end
+
+    def test_scan_pairs__size_0
+      res = []
+      ExpenseReportFix.scan_pair_combinations(0) { |i, j| res << [i, j] }
+      assert_equal [], res
+    end
+
+    def test_scan_pairs__size_1
+      res = []
+      ExpenseReportFix.scan_pair_combinations(1) { |i, j| res << [i, j] }
+      assert_equal [], res
+    end
+
+    def test_scan_triads__size_4
+      res = []
+      ExpenseReportFix.scan_triad_combinations(4) { |i, j, k| res << [i, j, k] }
+      assert_equal [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]], res
+    end
+
+    def test_scan_triads__size_5
+      res = []
+      ExpenseReportFix.scan_triad_combinations(5) { |i, j, k| res << [i, j, k] }
+      assert_equal 10, res.size
+    end
+
+    def test_scan_triads__size_0
+      res = []
+      ExpenseReportFix.scan_pair_combinations(0) { |i, j| res << [i, j] }
+      assert_equal [], res
+    end
+
+  end
+
+  class TestFindMatchingPairMethod < Test::Unit::TestCase
 
     test "day1 - reference input" do
-      assert_equal 514579, ExpenseReportFix.scan_pairs(REFERENCE_INPUT)
+      assert_equal [1721, 299], ExpenseReportFix.find_matching_pair(REFERENCE_INPUT)
     end
 
     test "day1 - no solution input" do
       input = [10, 11, 12, 13, 14]
-      assert_nil ExpenseReportFix.scan_pairs(input)
+      assert_nil ExpenseReportFix.find_matching_pair(input)
     end
 
     test "day1 problem input" do
       # print "Answer: [#{ExpenseReports.fix_pairs TRUE_INPUT}]\n"
-      assert_equal 1006176, ExpenseReportFix.scan_pairs(PUZZLE_INPUT)
+      assert_equal [892, 1128], ExpenseReportFix.find_matching_pair(PUZZLE_INPUT)
     end
   end
 
-  class TestExpenseFixesDay2 < Test::Unit::TestCase
+  class TestFindMatchingTriadMethod < Test::Unit::TestCase
 
-    test "triads - reference test data" do
-      assert_equal 241861950, ExpenseReportFix.scan_triads(REFERENCE_INPUT)
+    test "triads - solve reference input" do
+      assert_equal [979, 366, 675], ExpenseReportFix.find_matching_triad(REFERENCE_INPUT)
     end
 
-    test "triads - no solution input" do
+    test "triads - return nil if no match" do
       input = [10, 11, 12, 13, 14]
-      assert_nil ExpenseReportFix.scan_triads(input)
+      assert_nil ExpenseReportFix.find_matching_triad(input)
     end
 
-    test "triads - problem input" do
+    test "triads - solve problem input" do
       # print "Answer: [#{ExpenseReports.fix_triads TRUE_INPUT}]\n"
-      assert_equal 199132160, ExpenseReportFix.scan_triads(PUZZLE_INPUT)
+      assert_equal [874, 890, 256], ExpenseReportFix.find_matching_triad(PUZZLE_INPUT)
     end
   end
 
-  #: Test count_valid_passwords method
+  #: Test ExpenseReportFix.main method
   class TestMainWithRefInput < Test::Unit::TestCase
 
     def test_cardinality_pairs
