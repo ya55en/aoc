61595899ac38ea6ce63d074d2e02c1e976eb74f0
Yassen Damyanov <yd@itlabs.bg>
2021-11-24 09:54:49 +0200

    Refactor Day2 solution (mostly renames)

--- tests/day2-tests/test_scan_passwds.rb
+++ tests/day2-tests/test_scan_passwds.rb
@@ -4,39 +4,39 @@ require 'test/unit'
 require 'stringio'
 
 require_relative '../common'
-require_relative SRC_DIR + 'day2/scan_psswds'
+require_relative SRC_DIR + 'day2/scan_passwds'
 
 # Need to isolate in a module to avoid constants' name collisions/side effects
-module TestScanPsswdsWrapper
+module TestScanPasswdsWrapper
 
   MY_DIR = File.expand_path File.dirname(__FILE__)
   REF_INPUT_FILE = MY_DIR + '/ref-input-day2.txt'
   PUZZLE_INPUT_FILE = SRC_DIR + 'day2/puzzle-input-day2.txt'
 
-  #: Test match_constraints? method
+  #: Test match_constraints_xxx? methods
   class TestMatchConstraints < Test::Unit::TestCase
 
     def test_orig_policy_positive_cases
-      assert_true ScanPsswds.match_constraints_orig?(1, 3, 'a', 'abcde')
-      assert_true ScanPsswds.match_constraints_orig?(2, 9, 'c', 'ccccccccc')
-      assert_true ScanPsswds.match_constraints_orig?(8, 9, 'c', 'c1c2c3c4c5c6c7c8')
+      assert_true ScanPasswds.match_constraints_orig?(1, 3, 'a', 'abcde')
+      assert_true ScanPasswds.match_constraints_orig?(2, 9, 'c', 'ccccccccc')
+      assert_true ScanPasswds.match_constraints_orig?(8, 9, 'c', 'c1c2c3c4c5c6c7c8')
     end
 
     def test_orig_policy_negative_cases
-      assert_false ScanPsswds.match_constraints_orig?(1, 3, 'b', 'cdefg')
-      assert_false ScanPsswds.match_constraints_orig?(2, 3, 'a', 'abcde')
-      assert_false ScanPsswds.match_constraints_orig?(2, 9, 'c', 'cccccccccac')
-      assert_false ScanPsswds.match_constraints_orig?(8, 9, 'c', 'c1c2c3c4c5c6c')
+      assert_false ScanPasswds.match_constraints_orig?(1, 3, 'b', 'cdefg')
+      assert_false ScanPasswds.match_constraints_orig?(2, 3, 'a', 'abcde')
+      assert_false ScanPasswds.match_constraints_orig?(2, 9, 'c', 'cccccccccac')
+      assert_false ScanPasswds.match_constraints_orig?(8, 9, 'c', 'c1c2c3c4c5c6c')
     end
 
     def test_tcp_policy_positive_cases
-      assert_true ScanPsswds.match_constraints_tcp?(1, 3, 'a', 'abcde') # found on first idx
-      assert_true ScanPsswds.match_constraints_tcp?(2, 9, 'c', 'cxccccccc') # found on second idx
+      assert_true ScanPasswds.match_constraints_tcp?(1, 3, 'a', 'abcde') # found on first idx
+      assert_true ScanPasswds.match_constraints_tcp?(2, 9, 'c', 'cxccccccc') # found on second idx
     end
 
     def test_tcp_policy_negative_cases
-      assert_false ScanPsswds.match_constraints_tcp?(1, 3, 'b', 'cdefg') # not found at all
-      assert_false ScanPsswds.match_constraints_tcp?(2, 9, 'c', 'ccccccccc') # found on both indices
+      assert_false ScanPasswds.match_constraints_tcp?(1, 3, 'b', 'cdefg') # not found at all
+      assert_false ScanPasswds.match_constraints_tcp?(2, 9, 'c', 'ccccccccc') # found on both indices
     end
 
   end
@@ -45,13 +45,13 @@ module TestScanPsswdsWrapper
   class TestPasswdValidForOrigPolicy < Test::Unit::TestCase
 
     def test_positive_cases
-      assert_true ScanPsswds.passwd_valid? '1-3 a: abcde', :match_constraints_orig?
-      assert_true ScanPsswds.passwd_valid? '2-9 c: ccccccccc', :match_constraints_orig?
+      assert_true ScanPasswds.passwd_valid? '1-3 a: abcde', :match_constraints_orig?
+      assert_true ScanPasswds.passwd_valid? '2-9 c: ccccccccc', :match_constraints_orig?
     end
 
     def test_negative_cases
-      assert_false ScanPsswds.passwd_valid? '1-3 b: cdefg', :match_constraints_orig?
-      assert_false ScanPsswds.passwd_valid? '8-9 c: c1c2c3c4c5c6c', :match_constraints_orig?
+      assert_false ScanPasswds.passwd_valid? '1-3 b: cdefg', :match_constraints_orig?
+      assert_false ScanPasswds.passwd_valid? '8-9 c: c1c2c3c4c5c6c', :match_constraints_orig?
     end
   end
 
@@ -59,13 +59,13 @@ module TestScanPsswdsWrapper
   class TestPasswdValidForTcpPolicy < Test::Unit::TestCase
 
     def test_positive_cases
-      assert_true ScanPsswds.passwd_valid? '1-3 a: abcde', :match_constraints_tcp?
-      assert_true ScanPsswds.passwd_valid? '8-9 c: c1c2c3c4c5c6c', :match_constraints_tcp?
+      assert_true ScanPasswds.passwd_valid? '1-3 a: abcde', :match_constraints_tcp?
+      assert_true ScanPasswds.passwd_valid? '8-9 c: c1c2c3c4c5c6c', :match_constraints_tcp?
     end
 
     def test_negative_cases
-      assert_false ScanPsswds.passwd_valid? '1-3 b: cdefg', :match_constraints_tcp?
-      assert_false ScanPsswds.passwd_valid? '2-9 c: ccccccccc', :match_constraints_tcp?
+      assert_false ScanPasswds.passwd_valid? '1-3 b: cdefg', :match_constraints_tcp?
+      assert_false ScanPasswds.passwd_valid? '2-9 c: ccccccccc', :match_constraints_tcp?
     end
   end
 
@@ -75,7 +75,7 @@ module TestScanPsswdsWrapper
     def test_count_valid_passwords
       ref_input = File.read(REF_INPUT_FILE)
       fake_io = StringIO.new(ref_input)
-      assert_equal 2, ScanPsswds.count_valid_passwords(fake_io, :match_constraints_orig?)
+      assert_equal 2, ScanPasswds.count_valid_passwords(fake_io, :match_constraints_orig?)
     end
   end
 
@@ -85,7 +85,7 @@ module TestScanPsswdsWrapper
     def test_count_valid_passwords
       ref_input = File.read(REF_INPUT_FILE)
       fake_io = StringIO.new(ref_input)
-      assert_equal 1, ScanPsswds.count_valid_passwords(fake_io, :match_constraints_tcp?)
+      assert_equal 1, ScanPasswds.count_valid_passwords(fake_io, :match_constraints_tcp?)
     end
   end
 
@@ -95,7 +95,7 @@ module TestScanPsswdsWrapper
     def test_default_policy # default policy is :orig
       res = nil
       output = TestHelpers.with_captured_stdout do
-        res = ScanPsswds.main [REF_INPUT_FILE]
+        res = ScanPasswds.main [REF_INPUT_FILE]
       end
       assert_equal 0, res
       assert_equal 'Total valid passwds: 2', output.strip
@@ -104,7 +104,7 @@ module TestScanPsswdsWrapper
     def test_orig_policy
       res = nil
       output = TestHelpers.with_captured_stdout do
-        res = ScanPsswds.main [REF_INPUT_FILE, '-m', 'orig']
+        res = ScanPasswds.main [REF_INPUT_FILE, '-m', 'orig']
       end
       assert_equal 0, res
       assert_equal 'Total valid passwds: 2', output.strip
@@ -113,27 +113,25 @@ module TestScanPsswdsWrapper
     def test_tcp_policy
       res = nil
       output = TestHelpers.with_captured_stdout do
-        res = ScanPsswds.main ['-m', 'tcp', REF_INPUT_FILE]
+        res = ScanPasswds.main ['-m', 'tcp', REF_INPUT_FILE]
       end
       assert_equal 0, res
       assert_equal 'Total valid passwds: 1', output.strip
     end
 
     def test_orig_policy_puzzle_input
-      puts PUZZLE_INPUT_FILE.inspect
       res = nil
       output = TestHelpers.with_captured_stdout do
-        res = ScanPsswds.main ['-m', 'orig', PUZZLE_INPUT_FILE]
+        res = ScanPasswds.main ['-m', 'orig', PUZZLE_INPUT_FILE]
       end
       assert_equal 0, res
       assert_equal 'Total valid passwds: 458', output.strip
     end
 
     def test_tcp_policy_puzzle_input
-      puts PUZZLE_INPUT_FILE.inspect
       res = nil
       output = TestHelpers.with_captured_stdout do
-        res = ScanPsswds.main ['-m', 'tcp', PUZZLE_INPUT_FILE]
+        res = ScanPasswds.main ['-m', 'tcp', PUZZLE_INPUT_FILE]
       end
       assert_equal 0, res
       assert_equal 'Total valid passwds: 342', output.strip
